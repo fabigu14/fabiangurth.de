76a40186c04e21d1c8d861ca838f1634faec6f93
Fabian Gurth <fabigurth@googlemail.com>
2021-10-19 16:52:29 +0200

    anchor + scroll to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fabiangurth';
+
+  scroll(anchor: string): void {
+    console.log(anchor);
+    let el = document.getElementById(anchor);
+    el.scrollIntoView({behavior:"smooth"});
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  
+  @Output() anchorElement = new EventEmitter<string>();
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  setAnchor(anchor: string) {
+    this.anchorElement.emit(anchor);
+  }
 }
